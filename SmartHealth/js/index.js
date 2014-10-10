044578b197fe6de1ab09a8122fc4af5884ad4975
heidyhb <heidy.hernandezb@pucp.pe>
2014-10-10 18:13:18 -0500

    agregada vista detalle de receta

--- SmartHealth/js/index.js
+++ SmartHealth/js/index.js
@@ -8,10 +8,10 @@ var data2={
 			"nombre":"Batido de Fresa",
 			"palabras_clave":["fresa","batido","desayuno"],
 			"categorias":["1","2","5"],
-			"instrucciones":["En una batidora, combinar leche, pl·tano y fresas. Agregar vainilla y az˙car de manera opcional. Bator hasta obtener la consistencia deseada. Verter en vasos y servir."],
+			"instrucciones":["En una batidora, combinar leche, pl√°tano y fresas. Agregar vainilla y az√∫car de manera opcional. Bator hasta obtener la consistencia deseada. Verter en vasos y servir."],
 			"imagen":"01_batido_fresas.jpg",
 			"video":"",
-			"evaluaciÛn":"4.5",
+			"evaluaci√≥n":"4.5",
 			"porciones": 2,
 			"calorias":236,
 			"num_usuarios":832,
@@ -23,10 +23,10 @@ var data2={
 			"nombre":"Langostinos Szechwan",
 			"palabras_clave":["langostinos","mariscos","szechwan"],
 			"categorias":["6"],
-			"instrucciones":["En un bowl, mezclar agua, ketchup, salsa de soya, maicena, miel, pimienta roja molida y jengibre. Dejar reservado.","Calentar aceite en una sartÈn grande a fuego medio-alto. Poner cebollas y ajo y cocinar por 30 segundos. Agregar los langostinos y revolver para cubrir con aceite. Agregar a la salsa y cocinar revolviendo hasta que la salsa se encuentre burbujeando y espese."],
+			"instrucciones":["En un bowl, mezclar agua, ketchup, salsa de soya, maicena, miel, pimienta roja molida y jengibre. Dejar reservado.","Calentar aceite en una sart√©n grande a fuego medio-alto. Poner cebollas y ajo y cocinar por 30 segundos. Agregar los langostinos y revolver para cubrir con aceite. Agregar a la salsa y cocinar revolviendo hasta que la salsa se encuentre burbujeando y espese."],
 			"imagen":"02_langostinos_szechwan.jpg",
 			"video":"",
-			"evaluaciÛn":"4.5",
+			"evaluaci√≥n":"4.5",
 			"porciones": 4,
 			"calorias":142,
 			"num_usuarios":2393,
@@ -39,12 +39,12 @@ var data2={
 			"palabras_clave":["tofu","espinaca","quiche","vegetariano"],
 			"categorias":["2","5","8"],
 			"instrucciones":["Precalentar el horno a 175 grados Celcius.",
-							"En una batidora, combinar el tofu y la leche; procesar hasta que estÈ cremoso aÒadiendo m·s leche de ser necesario. AÒadir sal y pimienta.",
+							"En una batidora, combinar el tofu y la leche; procesar hasta que est√© cremoso a√±adiendo m√°s leche de ser necesario. A√±adir sal y pimienta.",
 							"En un bowl mediano, combinar espinaca, ajo, cebolla, queso cheddar, queso suizo y la mezcla de tofu. Mezclar bien y poner en un molde de pie preparado.",
-							"Cocinar en el horno precalentado por 30 minutos o hasta que estÈ dorado en la superficie. Dejar reposar por 5 minutos antes de cortar."],
+							"Cocinar en el horno precalentado por 30 minutos o hasta que est√© dorado en la superficie. Dejar reposar por 5 minutos antes de cortar."],
 			"imagen":"03_quiche_tofu.jpg",
 			"video":"",
-			"evaluaciÛn":"4.5",
+			"evaluaci√≥n":"4.5",
 			"porciones": 6,
 			"calorias":288,
 			"num_usuarios":152,
@@ -56,10 +56,10 @@ var data2={
 			"nombre":"Ensalada de Feta y Col Rizada",
 			"palabras_clave":["feta","col","ensalada","vegetariano"],
 			"categorias":["2","3","5","7"],
-			"instrucciones":["Mezclar la col con sal en un bowl por 2 minutos. Poner vinagre sobre la col y revolver. Trozar manzana, queso feta y piÒones a la col."],
+			"instrucciones":["Mezclar la col con sal en un bowl por 2 minutos. Poner vinagre sobre la col y revolver. Trozar manzana, queso feta y pi√±ones a la col."],
 			"imagen":"04_ensalada_feta_kale.jpg",
 			"video":"",
-			"evaluaciÛn":"5",
+			"evaluaci√≥n":"5",
 			"porciones": 8,
 			"calorias":43,
 			"num_usuarios":832,
@@ -71,11 +71,11 @@ var data2={
 			"nombre":"Estofado de carne",
 			"palabras_clave":["carne","estofado"],
 			"categorias":["4"],
-			"instrucciones":["En una olla grande, cocinad la carne en aceite a fuego lento hasta que estÈ cocida. Disolver caldo en agua y verterlo en la olla. Esparcir romero, perejil y pimienta. Llevar a punto de ebulliciÛn y luego reducir la temperatura, cubrir la olla y cocer por 1 hora.",
-							"Poner papas, zanahorias, apio y cebolla dentro de la olla. Disolver maicena en 2 cucharaditas de agua frÌa y echar al estofado. Cubrir y dejas cocinar por 1 hora m·s."],
+			"instrucciones":["En una olla grande, cocinad la carne en aceite a fuego lento hasta que est√© cocida. Disolver caldo en agua y verterlo en la olla. Esparcir romero, perejil y pimienta. Llevar a punto de ebullici√≥n y luego reducir la temperatura, cubrir la olla y cocer por 1 hora.",
+							"Poner papas, zanahorias, apio y cebolla dentro de la olla. Disolver maicena en 2 cucharaditas de agua fr√≠a y echar al estofado. Cubrir y dejas cocinar por 1 hora m√°s."],
 			"imagen":"05_estofado.jpg",
 			"video":"",
-			"evaluaciÛn":"4.5",
+			"evaluaci√≥n":"4.5",
 			"porciones": 10,
 			"calorias":401,
 			"num_usuarios":1790,
@@ -84,15 +84,15 @@ var data2={
 		},
 		{
 			"id":"06",
-			"nombre":"Keke de pl·tano",
-			"palabras_clave":["pl·tano","keke"],
+			"nombre":"Keke de pl√°tano",
+			"palabras_clave":["pl√°tano","keke"],
 			"categorias":["9"],
-			"instrucciones":["Precalentar el horno a 175 grados Celcius. Engrasar un poco una sartÈn de 9x5 pulgadas.",
-							"En un bowl grande, combinar la harina, el polvo de hornear y la sal. En un bowl separado, mezclar la mantequilla y el az˙car rubia. Agregar los huevos y las bananas hasta que se encuentre bien mezclado. Unir la mezcla de las bananas y harina. Poner mantequilla en la sartÈn preparado.",
+			"instrucciones":["Precalentar el horno a 175 grados Celcius. Engrasar un poco una sart√©n de 9x5 pulgadas.",
+							"En un bowl grande, combinar la harina, el polvo de hornear y la sal. En un bowl separado, mezclar la mantequilla y el az√∫car rubia. Agregar los huevos y las bananas hasta que se encuentre bien mezclado. Unir la mezcla de las bananas y harina. Poner mantequilla en la sart√©n preparado.",
 							"Cocinar en el horno precalentado de 60 a 65 minutos. Dejar enfriar en el molde por 10 minutos, luego voltearlo en otro recipiente."],
 			"imagen":"06_banana_bread.jpg",
 			"video":"",
-			"evaluaciÛn":"4.5",
+			"evaluaci√≥n":"4.5",
 			"porciones": 8,
 			"calorias":229,
 			"num_usuarios":10506,
@@ -169,16 +169,47 @@ $(document).ready($(function ()
   
 	paintRecipes(3);
  
-    
+     /*$(document).keydown(function(){
+  		alert("hola")});*/
+	$('.detalle-receta').mousedown(function(){
+		var id = $(this).attr("id_receta");
+  		selectRecipe(id)});
+
+	$('#volverBtn').click(function(){
+  		$('#recipe-details').addClass("invisible-block");  		
+    });
+
+    $('#favoritosBtn').click(function(){
+  		alert("Adicionar a favoritos!"); 		
+    });  
+
+    $('#voicePlayBtn').click(function(){
+    	var audio = new Audio();
+    	var playText = text.substring(0,99);
+    	audio.src ='http://translate.google.com/translate_tts?ie=utf-8&tl=es&q=' + encodeURI(playText);
+		audio.play();
+    	/*for (var i = 0; i <= text.length / 100;i++) {
+    		playText = text.substring(i*100,i*100+99);
+    		alert(playText);
+    		audio.src ='http://translate.google.com/translate_tts?ie=utf-8&tl=es&q=' + encodeURI("hola" + i);
+			audio.play();
+			sleep(2000,alert());
+    	};*/
+    	
+    });
+
   })); 
   
-  
+  function sleep(millis, callback) {
+    setTimeout(function() { callback(); }, millis);
+  }
+
   function paintRecipes(numColumns){
 	var targetdiv=$('#resultadoRecetas')
     var recetaDiv="<table>";
     for ( var i=0; i<data2.recetas.length; i++ ) {
 		if(i%numColumns==0)	recetaDiv+='<tr>'
-        recetaDiv+= '<td><div id="receta_'+i+'" class="detalle-receta">';
+        recetaDiv+= '<td><div id="receta_'+i+'" class="detalle-receta" id_receta='+i+'>';
 		var puntuacion='<div id="star_'+i+'" class="rating">&nbsp;</div>';
 		var textoReceta='<div id=textReceta_'+i+' class="texto-detalle"><p>'+data2.recetas[i].nombre+'</p></div>';
 		var imagenReceta='<div id=imagenReceta_'+i+' class="imagen-detalle"><img src="images/'+data2.recetas[i].imagen+'" width="82 "height="76"></div>';
@@ -194,3 +225,208 @@ $(document).ready($(function ()
 	}
   
   }
+
+  var exampleRecipe = {
+    "name":"Langostinos Szechwan",
+    "keyword":"langostinos, mariscos, szechwan",
+    "rating":"4",
+    "raters":"2393",
+    "calories":"142",
+    "time":"20",
+    "difficulty":"3",
+    "fat":"4.4",
+    "carbohydrate":"6.7",
+    "fiber":"0.4",
+    "proteins":"18.3",
+    "cholesterol":"0.164",
+    "sodium":"0.5",
+    "created_at":"2014-10-08 12:16:03",
+    "hits":"0",
+    "categories":[
+      {
+        "id":"14",
+        "name":"Mariscos"
+      }
+    ],
+    "instructions":[
+      "En un bowl, mezclar agua, ketchup, salsa de soya, maicena, miel, pimienta roja molida y jengibre. Dejar reservado.",
+      "Calentar aceite en una sart√©n grande a fuego medio-alto. Poner cebollas y ajo y cocinar por 30 segundos. Agregar los langostinos y revolver para cubrir con aceite. Agregar a la salsa y cocinar revolviendo hasta que la salsa se encuentre burbujeando y espese."
+    ],
+    "ingredients":[
+      {
+        "id":"6",
+        "name":"agua",
+        "unit":"cucharada",
+        "quantity":"4",
+        "description":"NULL"
+      },
+      {
+        "id":"7",
+        "name":"ketchup",
+        "unit":"cucharada",
+        "quantity":"2",
+        "description":"NULL"
+      },
+      {
+        "id":"8",
+        "name":"salsa de soya",
+        "unit":"cucharada",
+        "quantity":"1",
+        "description":"NULL"
+      },
+      {
+        "id":"9",
+        "name":"maicena",
+        "unit":"cucharadita",
+        "quantity":"2",
+        "description":"NULL"
+      },
+      {
+        "id":"10",
+        "name":"miel",
+        "unit":"cucharadita",
+        "quantity":"1",
+        "description":"NULL"
+      },
+      {
+        "id":"11",
+        "name":"pimienta roja",
+        "unit":"cucharadita",
+        "quantity":"0.5",
+        "description":"NULL"
+      },
+      {
+        "id":"12",
+        "name":"jenjibre",
+        "unit":"cucharadita",
+        "quantity":"0.25",
+        "description":"NULL"
+      },
+      {
+        "id":"13",
+        "name":"aceite vegetal",
+        "unit":"cucharada",
+        "quantity":"1",
+        "description":"NULL"
+      },
+      {
+        "id":"14",
+        "name":"cebolla verde",
+        "unit":"taza",
+        "quantity":"0.25",
+        "description":"cortada"
+      },
+      {
+        "id":"15",
+        "name":"diente de ajo",
+        "unit":"unidad",
+        "quantity":"4",
+        "description":"troceado"
+      },
+      {
+        "id":"16",
+        "name":"langostino",
+        "unit":"gramos",
+        "quantity":"340",
+        "description":"cocinado, sin cola"
+      }
+    ]
+  };
+
+  var currentInstructions;
+
+  function selectRecipe(recipeId){
+  	//alert(recipeId);
+  	fillRecipeDetails(exampleRecipe);
+  	$('#recipe-details').removeClass("invisible-block");
+  	
+  	currentInstructions = exampleRecipe.instructions;
+  	text = "";
+  	for (var i = 0; i < currentInstructions.length; i++) {
+  		text += currentInstructions[i];
+  	};
+
+  	var soapMessage =
+    '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sexy="http://www.dreamsolutions.com/sexy_service/">' +
+   		'<soapenv:Header/>' +
+   			'<soapenv:Body>' +
+      			'<sexy:id_recipe>3</sexy:id_recipe>' +
+   			'</soapenv:Body>' +
+	'</soapenv:Envelope>';
+
+    $.ajax({
+    	url: "http://200.16.7.111/wordpress/index.php?/wpws/?wsdl",
+    	type: "POST",
+    	contentType: 'text/xml; charset=utf-8',
+    	headers: {
+        	SOAPAction: "http://www.dreamsolutions.com/sexy_service/smartSelectRecipeService"
+    	},
+    	data: soapMessage,
+    	success: function(soapResponse){
+        	//DO SOMETHING
+        	alert("OK");
+        	//alert(soapResponse);
+        }
+        //error: alert("error")
+        
+    });
+	
+
+	/*$.soap({
+    	url: 'http://200.16.7.111/wordpress/index.php?/wpws/?wsdl',
+    	method: 'http://www.dreamsolutions.com/sexy_service/smartSelectRecipeService',
+    	data: {
+        	id_recipe: '2'
+    	},
+    	success: function (soapResponse) {
+    		alert(soapResponse);
+        // do stuff with soapResponse
+    	},
+    	error: function (soapResponse) {
+        	alert('that other server might be down...');
+    	}
+	});*/
+  	//alert("end");
+  }
+
+  function fillRecipeDetails(details){
+  	$('#recipeTitle').html(details.name);
+  	
+  	//ingredientes
+  	var ingredientes = details.ingredients;
+  	var rowDisplay;
+  	for (var i = 0; i < ingredientes.length; i++) {
+  		rowDisplay = '<tr> <td class="ingredientCell">' + ingredientes[i].name + '</td> <td class="umCell">' + 
+  		ingredientes[i].unit + '</td> <td class="quantityCell">' + ingredientes[i].quantity + '</td> </tr>';
+  		$('#ingredientsTable').append(rowDisplay);
+  	}
+  	
+  	//instrucciones
+  	var instrucciones = details.instructions;
+  	var pasosDisplay = "Instrucciones: <br/>";
+  	for (var i = 0; i < instrucciones.length; i++) {
+  		pasosDisplay += "<br/>" + (i + 1) + ". " + instrucciones[i] +" <br/>";
+  	}
+  	$('#preparationDiv').html(pasosDisplay);
+
+  	// dificultad
+  	$('#dificultad').html("Dificultad: " + details.difficulty);
+  	
+  	// tiempo
+  	$('#duracion').html("Tiempo de preparaci√≥n: " + details.time + " minutos");
+  	
+  	// informacion nutricional
+  	var nutriDisplay = "Informaci√≥n nutricional: <br/>";
+  	nutriDisplay += "Calor√≠as: " + details.calories + " kcal <br/>";
+  	nutriDisplay += "Colesterol: " + details.cholesterol + " g <br/>";
+  	nutriDisplay += "Fibra: " + details.fiber + " g <br/>";
+  	nutriDisplay += "Sodio: " + details.sodium + " g <br/>";
+  	nutriDisplay += "Carbohidratos: " + details.carbohydrate + " g <br/>";
+  	nutriDisplay += "Grasas: " + details.fat + " g <br/>";
+  	nutriDisplay += "Proteinas: " + details.proteins + " g <br/>";
+  	$('#infoNutricional').html(nutriDisplay);
+
+  	// imagen (provisional)
+  	var imgDisplay= '<img id="recipeImag" src="images/02_langostinos_szechwan.jpg"/>';
+  	$('#recipePhoto').html(imgDisplay);
+  }	
